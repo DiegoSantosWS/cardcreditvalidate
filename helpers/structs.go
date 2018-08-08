5ecc4a6ec407f2aecfc7f688f57c345d7de2f4c8
DiegoSantosWS <tec.infor321@gmail.com>
2018-08-08 19:07:50 -0300

    criado nova struct para inserir a compania do cartão

--- helpers/structs.go
+++ helpers/structs.go
@@ -2,6 +2,11 @@ package helpers
 
 //Messege retorna messagem
 type Message struct {
-	Msg string `json:"msg"`
-	Err bool   `json:"err,omitempty"`
+	Msg  string  `json:"msg"`
+	Err  bool    `json:"err,omitempty"`
+	Comp Company `json:"comp,omitempty"`
+}
+
+type Company struct {
+	Short, Long string
 }
